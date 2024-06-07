cecdb1f522f23637c2635b031258f1457f7c9ad3
Brennon Fitzpatrick <tf@tullyfitzpatrick.com>
2024-06-07 10:20:56 -0400

    Changed navbar up

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -11,18 +11,32 @@ import {
 
 export default function Navbar() {
   return(
-    <nav>
-      <Image className="h-auto w-20"
-        src= {Logo}
-        alt="Revline Logo" 
-        placeholder = 'blur'    
-      />
-      <Link href="/">Home</Link>
-      <Link href="/odds">Odds</Link>
-      {/* <Link href="/predictor">Predictor</Link> */}
-      <Link href="/arbitrage">Arbitrage</Link>
-      <Link href="/analytics">Analytics </Link>
-      <UserButton />
-    </nav>
+    <nav className="w-full">
+      <div className="container mx-auto p-4 flex items-center justify-between">
+        <Image
+          className="h-auto w-20"
+          src={Logo}
+          alt="Revline Logo"
+          placeholder="blur"
+        />
+        <div className="flex space-x-4">
+          <Link href="/">Home</Link>
+          <Link href="/odds">Odds</Link>
+          {/* <Link href="/predictor">Predictor</Link> */}
+          <Link href="/arbitrage">Arbitrage</Link>
+          <Link href="/analytics">Analytics</Link>
+          <Link href="/blockchain">Blockchain</Link>
+        </div>
+        <UserButton
+            appearance={{
+              elements: {
+                userButtonAvatarBox: "h-12 w-12", // Adjust size of avatar box
+                userButton: "p-4", // Add padding to user button
+                userButtonAvatar: "text-lg", // Adjust text size if applicable
+              },
+            }}
+          />
+      </div>
+</nav>
   )
 }
